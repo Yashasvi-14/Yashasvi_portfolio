19081ea1f0354968f270776e7619fe1091a70d29
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-06-24 14:09:51 +0530

    Added topButton,scroll reveal animation with AOS.js

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const toggleBtn= document.getElementById("darkModeToggle");
 toggleBtn.addEventListener("click",()=>{
     document.body.classList.toggle("dark-mode");
 });
+
+const topBtn=document.getElementById("topBtn");
+window.onscroll=function(){
+    if(document.body.scrollTop>200 || document.documentElement.scrollTop > 200)
+    {
+        topBtn.style.display="block";//show the button
+    }
+    else{
+        topBtn.style.display="none";
+    }
+};
+
+function scrollToTop(){
+    window.scrollTo({top: 0,behavior: 'smooth'});
+}
